6d580174030cebce11246f04a30cd0d9e8f6a4ca
Ben Calebe Dhein Hagmayer <bencalebe5@gmail.com>
2026-01-19 23:09:57 -0300

    Estudo dos arquivos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-red-600 flex items-center justify-center">
       <h1 className="text-4xl md:text-6xl font-bold text-white">Olá Mundo</h1>
+      <p className="text-10xl text-green-500">calebe</p>
     </main>
   );
 }
