1e0736836974ddbc849a297a3f5844cd26aaa7ed
Ben Calebe Dhein Hagmayer <bencalebe5@gmail.com>
2026-01-21 11:15:18 -0300

    animação da navegação finalizada

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,7 @@
-// Header Component
-// Navegação principal do site
+"use client";
 
 import React from "react";
+import { motion } from "framer-motion";
 import BotãoRede from "../ui/Button";
 import Navigation from "./Navigation";
 
@@ -10,19 +10,44 @@ interface HeaderProps {
 }
 
 export default function Header({}: HeaderProps) {
-  return (
-    // Adicionei w-full para o header ocupar a largura da tela e permitir o afastamento
-    <header className="fixed top-0 left-0 w-full p-6 z-50">
-      <nav className="flex items-center justify-between max-w-7xl mx-auto">
-        <div>
-          <Navigation />
-        </div>
-        <div className="flex items-center gap-3">
-          <BotãoRede titulo="whatsapp" link="/" icone="/iconss/whatsicon.png"/>
-          <BotãoRede titulo="Instagram" link="/" icone="/iconss/instaicon.png" />
-        </div>
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2, // Tempo entre a entrada de cada item (0.2 segundos)
+      },
+    },
+  };
+  // 2. Configuração da animação de cada "Filho" (Botões/Nav)
+  const itemVariants = {
+    hidden: { opacity: 0, y: -20 }, // Começa invisível e 20px acima
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring", // Efeito de mola (suave)
+        stiffness: 100,
+      },
+    },
+  };
 
-      </nav>
+  return (
+    <header className="fixed top-0 left-0 w-full p-6 z-50">
+      <motion.nav
+        initial="hidden"
+        animate="visible"
+        variants={containerVariants}
+        className="flex justify-between items-center"
+      >
+        <motion.div variants={itemVariants}>
+          <Navigation />
+        </motion.div>
+        <motion.div variants={itemVariants} className="flex items-center gap-3">
+          <BotãoRede titulo="whatsapp" link="/" icone="/iconss/whatsicon.png" />
+          <BotãoRede titulo="Instagram" link="/" icone="/iconss/instaicon.png" />
+        </motion.div>
+      </motion.nav>
     </header>
   );
 }
